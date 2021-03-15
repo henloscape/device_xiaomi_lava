04a9224eac591cc449b92e559aa3a133d99fc8b5
JamieHoSzeYui <henloboii@protonmail.ch>
2021-03-15 12:39:23 +0800

    lava: Flipendo: refractor apk path use

--- Flipendo/Android.mk
+++ Flipendo/Android.mk
@@ -1,7 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := Flipendo
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_SRC_FILES := Flipendo.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_SYSTEM_EXT_MODULE := true
