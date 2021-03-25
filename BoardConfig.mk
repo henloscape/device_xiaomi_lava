d6b83d0b872d54bfc9530f3172e670222e295daf
JamieHoSzeYui <henloboii@protonmail.ch>
2021-03-25 18:31:43 +0800

    [DNM] lava: skip ABI checks

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,10 @@
 
 DEVICE_PATH := device/xiaomi/lava
 
+# Broken shit
 BUILD_BROKEN_DUP_RULES := true
+SKIP_ABI_CHECKS := true 
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
